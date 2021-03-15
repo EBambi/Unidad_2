926dc9f0d38edaa0de37ce46f362550a04830354
Bambi <esteban.gonzalezg@upb.edu.co>
2021-03-15 18:27:56 -0500

    ejercicio7

---
+++ ejercicio7/exec.c
@@ -0,0 +1,14 @@
+//EXEC.c
+
+#include<stdio.h>
+#include<unistd.h>
+
+int main()
+{
+    int i;
+
+    printf("I am EXEC.c called by execv() ");
+    printf("\n");
+    printf("Esto es una prueba\n");
+    return 0;
+}
