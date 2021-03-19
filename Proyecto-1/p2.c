f36299a5e0f9e1b6f78f5e17eb0397db4c267094
Bambi <esteban.gonzalezg@upb.edu.co>
2021-03-19 15:24:21 -0500

    Proyecto1-p2Hecho

---
+++ Proyecto-1/p2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+FILE *inFile, *outFile;
+int ch;
+
+int main(int argc, char const *argv[]) {
+    int i=0, vals[100], valor, n=100, q, j, temp;
+    char keys[100][20], key[20], tempC[20];
+
+    inFile = fopen(argv[1],"r");
+    if(inFile == NULL){
+        printf("No se puede abrir %s\n",argv[1]);
+        exit(8);
+    }
+    while (!feof(inFile))
+    {
+        if(ch != EOF){
+            fscanf(inFile,"%s %d",key,&vals[i]);
+            strcpy(keys[i],key);
+            //printf("Clave: %s Valor: %d\n",keys[i],vals[i]);
+            i++;
+        }
+    }
+    fclose(inFile);
+    outFile = fopen(argv[2],"w");
+    for (int r = i-1; r >= 0; r--)
+    {
+        fprintf(outFile,"%s %d\n",keys[r],vals[r]);
+    }
+    fclose(outFile);
+
+    return EXIT_SUCCESS;
+}
